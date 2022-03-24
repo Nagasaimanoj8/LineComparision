da34b87ac9c97f2d428af7bfe9a717dd0fc901c6
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-24 07:16:04 +0530

    Welcoe

---
+++ Welcome.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComarision
+{
+    internal class Welcome
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
